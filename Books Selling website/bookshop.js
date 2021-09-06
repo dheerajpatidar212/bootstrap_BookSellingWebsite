ccebfc0671acc487a5b5fcddd0b2a408ad6217e2
Dheeraj Patidar <88872393+dheerajpatidar212@users.noreply.github.com>
2021-09-06 21:43:43 +0530

    Book Selling Website using Bootstrap

---
+++ Books Selling website/bookshop.js
@@ -0,0 +1,4 @@
+function signup_greeting(){
+    document.getElementById('signup-modal-body').innerHTML=
+    "<strong>Thanks For Joining us!</strong>\n\n\nLorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium dolore laborum vel asperiores, nemo ab tempora sit veritatis, quisquam non hic totam deleniti at ratione perferendis quasi ex saepe officiis?"
+}
